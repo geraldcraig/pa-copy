ec7907befb5d4af361388b00384a19f2fcfbbc68
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2019-09-17 15:15:14 +0100

    Working with controllers

---
+++ 03-02-DwarfClass/src/uk/ac/belfastmet/Main.java
@@ -0,0 +1,59 @@
+package uk.ac.belfastmet;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+	
+		Dwarf sleepy = new Dwarf();
+		sleepy.setName("Sleepy");
+		sleepy.setAuthor("Disney");
+		dwarfs.add(sleepy);
+		
+		Dwarf happy = new Dwarf();
+		happy.setName("Happy");
+		happy.setAuthor("Disney");
+		dwarfs.add(happy);
+		
+		Dwarf doc = new Dwarf();
+		doc.setName("Doc");
+		doc.setAuthor("Disney");
+		dwarfs.add(doc);
+		
+		Dwarf thorin = new Dwarf();
+		thorin.setName("Thorin");
+		thorin.setAuthor("Tolkien");
+		dwarfs.add(thorin);
+		
+		Dwarf balin = new Dwarf();
+		balin.setName("Balin");
+		balin.setAuthor("Tolkien");
+		dwarfs.add(balin);
+		
+		System.out.println("The Disney Dwarfs are:");
+		
+		for (Dwarf dwarf:dwarfs) {
+			if(dwarf.getAuthor()== "Disney") {
+				System.out.println(dwarf.getName());
+				
+			}	
+			
+			
+		}
+		
+		System.out.println("The Tolkien Dwarfs are:");
+		
+		for (Dwarf dwarf:dwarfs) {
+			if(dwarf.getAuthor()== "Tolkien") {
+				System.out.println(dwarf.getName());
+			
+			}
+
+			
+		}
+	}
+}
+
