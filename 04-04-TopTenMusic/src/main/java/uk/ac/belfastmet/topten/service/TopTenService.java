5beb90541c7f53fea5c5ffe8eb8d039a905f853e
Gerald Craig <geraldcraig@me.com>
2019-09-19 08:33:13 +0100

    album artwork added

--- 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/service/TopTenService.java
+++ 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/service/TopTenService.java
@@ -19,11 +19,11 @@ public class TopTenService {
 		this.singlesTopTen = new ArrayList<Song>();
 		
 		//add singles here!
-		this.singlesTopTen.add(new Song("Take Me Back To London", "Ed Sheeran", "ed.png"));
-		this.singlesTopTen.add(new Song("Taste (Make It Shake", "Aitch", "ed.png"));
-		this.singlesTopTen.add(new Song("Higher Love", "Kygo & Whitney Houston", "ed.png"));
-		this.singlesTopTen.add(new Song("Ladbroke Grove", "AJ Tracey", "ed.png"));
-		this.singlesTopTen.add(new Song("Circles", "Post Malone", "ed.png"));
+		this.singlesTopTen.add(new Song("Take Me Back To London", "Ed Sheeran Ft. Stormzy", "edSheeran.png"));
+		this.singlesTopTen.add(new Song("Taste (Make It Shake)", "Aitch", "aitch.png"));
+		this.singlesTopTen.add(new Song("Higher Love", "Kygo & Whitney Houston", "kygo.png"));
+		this.singlesTopTen.add(new Song("Ladbroke Grove", "AJ Tracey", "ajTracey.png"));
+		this.singlesTopTen.add(new Song("Circles", "Post Malone", "postMalone.png"));
 		
 		
 		return this.singlesTopTen;
@@ -34,7 +34,12 @@ public class TopTenService {
 		this.albumsTopTen = new ArrayList<Album>();
 		
 		//add albums here!
-		this.albumsTopTen.add(new Album("", "Albums", ".png"));
+		this.albumsTopTen.add(new Album("Hollywood's Bleeding", "Post Malone", "postMaloneA.png"));
+		this.albumsTopTen.add(new Album("No.6 Collaborations Project", "Ed Sheeran", "edSheeranA.png"));
+		this.albumsTopTen.add(new Album("Aitch20", "Aitch", "aitchA.png"));
+		this.albumsTopTen.add(new Album("Lover", "Taylor Swift", "taylorSwiftA.png"));
+		this.albumsTopTen.add(new Album("Divinely Uninspired To A Hellish Extent", "Lewis Capaldi", "lewisCapaldiA.png"));
+		
 			
 		
 		return this.albumsTopTen;
