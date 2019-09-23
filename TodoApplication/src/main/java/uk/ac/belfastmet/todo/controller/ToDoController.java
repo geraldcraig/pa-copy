092064984089d31e857bb67bb564495ed214703d
Gerald Craig <geraldcraig@me.com>
2019-09-23 22:47:48 +0100

    page 3 added to to do list

--- TodoApplication/src/main/java/uk/ac/belfastmet/todo/controller/ToDoController.java
+++ TodoApplication/src/main/java/uk/ac/belfastmet/todo/controller/ToDoController.java
@@ -19,6 +19,13 @@ public class ToDoController {
 	
 	Logger log = LoggerFactory.getLogger(ToDoController.class);
 	
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
+	
 	@GetMapping("/")
 	public String homePage(Model model) {
 		
@@ -32,9 +39,14 @@ public class ToDoController {
 		log.info("index page");
 		
 		return "index";
-		
-		 
+			 
 	}
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
 	
 	@GetMapping("/Page2")
 	public String page2Page(Model model) {
@@ -48,12 +60,31 @@ public class ToDoController {
 		
 		log.info("page 2");
 		
-		return "page2";
-		
+		return "page2";	
 		 
 	}
 	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
+	
+	@GetMapping("/Page3")
+	public String page3Page(Model model) {
+		
+		ToDoService todoService = new ToDoService();
+		model.addAttribute("tasks", todoService.gettaskToDo());
+		
+		String name = "Gerald";
+		model.addAttribute("myName", name);
+		
+		
+		log.info("page 3");
+		
+		return "page3";
 	
 	
+	}
 
 }
