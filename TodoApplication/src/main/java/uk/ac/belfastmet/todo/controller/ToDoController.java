2c83bc0f3183e93b447d02bb61d6200277be6aa2
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2019-09-24 09:54:00 +0100

    comments added to To Do

--- TodoApplication/src/main/java/uk/ac/belfastmet/todo/controller/ToDoController.java
+++ TodoApplication/src/main/java/uk/ac/belfastmet/todo/controller/ToDoController.java
@@ -1,6 +1,5 @@
 package uk.ac.belfastmet.todo.controller;
 
-
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -11,80 +10,74 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jdk.internal.org.jline.utils.Log;
 import uk.ac.belfastmet.todo.service.ToDoService;
 
-
-
 @Controller
 @RequestMapping
 public class ToDoController {
-	
+
 	Logger log = LoggerFactory.getLogger(ToDoController.class);
-	
-	
+
 	/**
 	 * 
 	 * @param model
 	 * @return
 	 */
-	
+
 	@GetMapping("/")
 	public String homePage(Model model) {
-		
+
 		ToDoService todoService = new ToDoService();
 		model.addAttribute("tasks", todoService.gettaskToDo());
-		
+
 		String name = "Gerald";
 		model.addAttribute("myName", name);
-		
-		
-		log.info("index page");
-		
+		//added a + name to the logger so that it prints out the name of the person in the console
+		log.info("index page" + name);
+		log.debug("there is a possible path error page one is not able to load");
 		return "index";
-			 
+
 	}
-	
+
 	/**
 	 * 
 	 * @param model
 	 * @return
 	 */
-	
+
 	@GetMapping("/Page2")
 	public String page2Page(Model model) {
-		
+
 		ToDoService todoService = new ToDoService();
 		model.addAttribute("tasks", todoService.gettaskToDo());
-		
+
 		String name = "Gerald";
 		model.addAttribute("myName", name);
-		
-		
-		log.info("page 2");
-		
-		return "page2";	
-		 
+		//added a + name to the logger so that it prints out the name of the person in the console
+		log.info("page 2" + name);
+		log.debug("there is a possible path error page two is not able to load");
+		return "page2";
+
 	}
-	
+
 	/**
 	 * 
 	 * @param model
 	 * @return
 	 */
-	
+
 	@GetMapping("/Page3")
 	public String page3Page(Model model) {
-		
+
 		ToDoService todoService = new ToDoService();
 		model.addAttribute("tasks", todoService.gettaskToDo());
-		
+
 		String name = "Gerald";
 		model.addAttribute("myName", name);
-		
-		
-		log.info("page 3");
-		
+		//added a + name to the logger so that it prints out the name of the person in the console
+		log.info("page 3" + name);
+		log.debug("there is a possible path error page three is not able to load" + name);
+
 		return "page3";
-	
-	
+
 	}
 
 }
--- TodoApplication/src/main/java/uk/ac/belfastmet/todo/service/ToDoService.java
+++ TodoApplication/src/main/java/uk/ac/belfastmet/todo/service/ToDoService.java
@@ -2,32 +2,37 @@ package uk.ac.belfastmet.todo.service;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import uk.ac.belfastmet.todo.domain.Task;
 
 @Service
 public class ToDoService {
-	
+	// logger here for the array debugging
+	Logger log = LoggerFactory.getLogger(ToDoService.class);
 	private ArrayList<Task> taskToDo;
-	
-	
-	public ArrayList <Task> gettaskToDo() {
-		
+
+	public ArrayList<Task> gettaskToDo() {
+
 		this.taskToDo = new ArrayList<Task>();
-		//					  Task(String description, String date, Boolean status, String name, String priority)
-		this.taskToDo.add(new Task("1", "learn java", "23 Sep 19", true , "Gerald", "high"));
-		this.taskToDo.add(new Task("2", "learn html","24 Sep 19", false ,"Gerald", "high"));
-		this.taskToDo.add(new Task("3", "learn css","25 Sep 19", false ,"Gerald", "high"));
-		this.taskToDo.add(new Task("4", "learn javascript","26 Sep 19", false ,"Gerald", "high"));
-		this.taskToDo.add(new Task("5", "learn spring boot","27 Sep 19", false ,"Gerald", "high"));
+		// Task(String description, String date, Boolean status, String name, String
+		// priority)
+		this.taskToDo.add(new Task("1", "learn java", "23 Sep 19", true, "Gerald", "high"));
+		this.taskToDo.add(new Task("2", "learn html", "24 Sep 19", false, "Gerald", "high"));
+		this.taskToDo.add(new Task("3", "learn css", "25 Sep 19", false, "Gerald", "high"));
+		this.taskToDo.add(new Task("4", "learn javascript", "26 Sep 19", false, "Gerald", "high"));
+		this.taskToDo.add(new Task("5", "learn spring boot", "27 Sep 19", false, "Gerald", "high"));
+
+
 		
+		// log debug here for the debugger console to read to know if the array has been successfully read
+		log.debug("if array shows its working correctly if not theres an issue");
 		
-		
+		// returning the tasktodo array
 		return this.taskToDo;
-		
+
 	}
-	
-	
 
 }
