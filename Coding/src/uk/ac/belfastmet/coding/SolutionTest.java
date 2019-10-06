bf992c6b2a27d76582d1ebfea29b5090f7677b9c
Gerald Craig <geraldcraig@me.com>
2019-10-06 21:14:19 +0100

    tech test tests

---
+++ Coding/src/uk/ac/belfastmet/coding/SolutionTest.java
@@ -0,0 +1,14 @@
+package uk.ac.belfastmet.coding;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testSolution() {
+		fail("Not yet implemented");
+	}
+
+}
