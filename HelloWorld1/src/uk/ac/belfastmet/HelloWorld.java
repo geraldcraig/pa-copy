8c35875f221d20bc40d6c942ae640f1f4827eea5
Gerald Craig <geraldcraig@me.com>
2021-03-05 22:52:46 +0000

    revision updated

---
+++ HelloWorld1/src/uk/ac/belfastmet/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.belfastmet;
+
+/**
+ * @author geraldcraig
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
